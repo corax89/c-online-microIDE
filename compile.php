5fb49636256e07d5eccac929ca0ed30cce4b612a
corax89 <corax8989@yandex.ru>
2017-06-26 16:29:27 +0300

    первый

---
+++ compile.php
@@ -0,0 +1,64 @@
+<?php
+
+$start = microtime(true);
+$input = $_POST['input'];
+$optimization = $_POST['optimization'];
+$setcompiler = $_POST['setcompiler'];
+
+$out='';
+
+//выполняем скрипт если в запросе есть исходный код
+if(isset($_POST['text_box'])) {
+
+	$a = $_POST['text_box'];
+	$filename='tmp/program.c';
+	$myfile = fopen($filename, "w") or die("Невозможно открыть файл для записи!");
+
+	fwrite($myfile, $a);
+	fclose($myfile);
+    
+    //добавляем параметры оптимизации
+    switch($optimization){
+        case 1:
+            $optstring = ' -O2';
+            break;
+        case 2:
+            $optstring = ' -Os';
+            break;
+        default:
+            $optstring = ' -O0';
+    }
+    
+    //компилируем выбранным компилятором
+    if($setcompiler==0){
+	    exec('gcc '. $filename . $optstring . ' -o tmp/program 2>&1', $outputErr, $return_value);
+    }
+	else{
+	    exec('i586-mingw32msvc-gcc ' . $filename . $optstring . ' -o tmp/program.exe 2>&1', $outputErr, $return_value);
+	}
+    
+	//проверка на наличие ошибок компиляции
+	if($return_value!=0){
+		$out = $out . 'ERROR: ' . PHP_EOL . implode(PHP_EOL, $outputErr);
+	}
+	else{
+	    //разрешаем файлу выполняться
+		exec('chmod 777 '.'tmp/program');
+		$inputFile='tmp/output.txt';
+		exec ('chmod 777 '.$inputFile);
+		$file = fopen($inputFile, "w") or die("Невозможно открыть файл для записи!");
+		
+		fwrite($file, $input);
+		fclose($file);
+
+		exec('./tmp/program<'.$inputFile, $output, $return_value);
+		//получаем результат выполнения программы
+		$out = $out . PHP_EOL . implode(PHP_EOL, $output);
+		$time = microtime(true) - $start;
+        printf('Время компиляции %.4F сек.', $time);
+	}
+	unlink($filename);
+	unlink($inputFile);
+}
+
+echo $out;
---
+++ script.js
@@ -0,0 +1,50 @@
+var out          = document.getElementById('output');
+var btnCompile   = document.getElementById('compile');
+var btnHello     = document.getElementById('hello');
+var setCompiler  = document.getElementById("setcompiler");
+var optimization = document.getElementById("optimization");
+var programmLink = document.getElementById("link");
+
+btnCompile.addEventListener("click", sendToCompile);
+btnHello.addEventListener("click", loadHello);
+
+function sendToCompile(){
+    //собираем тело запроса
+    var body = 'setcompiler=' + encodeURIComponent(setCompiler.value) + '&optimization=' + encodeURIComponent(optimization.value) + '&text_box=' + encodeURIComponent(editAreaLoader.getValue("codebox"));
+    //создаем запрос
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'compile.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    //отправляем запрос
+    xhr.send(body); // (1)
+    xhr.onreadystatechange = function() { // (3)
+        if (xhr.readyState != 4) 
+            return;
+        //активируем кнопку компиляции
+        btnCompile.disabled = false;
+        if (xhr.status != 200) {
+            out.value = 'server error ' + xhr.status + ': ' + xhr.statusText;
+        } 
+        else {
+            out.value = xhr.responseText;
+            //меняем ссылку на программу в зависимости от выбранного компилятора
+            if (setCompiler.value == 0)
+                programmLink.href = 'tmp/program';
+            else
+                programmLink.href = 'tmp/program.exe';
+        }
+    }
+    //блокируем кнопки в ожидании ответа
+    out.value = 'Компиляция...'; // (2)
+    btnCompile.disabled = true;
+    programmLink.href = '#';
+}
+
+function loadHello(){
+    //загружаем в редактор текст программы Hello world
+    var s='#include <stdio.h>\n\n';
+    s+='int main (void){\n';
+    s+='\tprintf ("Hello, World!\\n");\n';
+    s+='\tgetchar ();\n\treturn 0;\n}\n';
+    editAreaLoader.setValue("codebox", s);
+}
